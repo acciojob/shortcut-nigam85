1ac4d976380967350afaa8abc4ad8eb2eb8cd755
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-10 14:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function shortcut(s1, s2) {
   // your code here
+	 if (s1.length === 0 || s2.length === 0) {
+        return '';
+    }
+
+	
+
+    // Get the initial letters of both strings
+    const initial1 = s1[0];
+    const initial2 = s2[0];
+
+    // Return the concatenated initial letters
+    return initial1 + initial2;
 }
 
 // Do not change the code below.
